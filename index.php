99556822dff6eabe8419f87fe60aec96bff5088d
Vilius Kukanauskas <admin@vilkas.de>
2015-10-03 11:47:19 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Vilkaz
+ * Date: 03.10.2015
+ * Time: 11:41
+ */
+
+require_once('vendor/autoload.php');
+
+echo "Hello ! jakshemash !";
+echo "Hello ! jakshemash !";
+
